3430d84d9016ef2bee1df7dbc3c54b7161bf6582
Anton Khassanov <marioanton9@gmail.com>
2023-11-28 12:25:02 -0500

    moc done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-// write your code here
+fetch("http://localhost:3000/ramens")
+.then(res => res.json())
+.then(ramens => {
+
+
+    ramens.forEach(ramen => {
+            const imgTag = document.createElement("img")
+            const divTag = document.querySelector("#ramen-menu")
+                imgTag.src = ramen.image
+
+                divTag.append(imgTag)
+    })
+
+       const button = document.querySelector("#ramen-menu")
+       
+     button.addEventListener('click', event => {
+            const imgTag = document.querySelector(".detail-image")
+            
+            imgTag.src = ramens.image
+
+                
+        
+     })
+        
+      
+       
+    
+}) 
+
+     
+ 
+          
+            
+            
+             
+                   
+       
+            
+          
+            
+        
+        
+        
+    
+        
+      
